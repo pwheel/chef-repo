c2bcc7a3009d78464f7bea4771959d021af3e43a
Paul Wheeler <pwheeler@LONC02J80K8DKQ4.sea.corp.expecn.com>
2013-06-30 22:50:40 +0100

    Import application_java version 1.1.0

---
+++ cookbooks/application_java/metadata.rb
@@ -0,0 +1,11 @@
+name             "application_java"
+maintainer       "ZephirWorks"
+maintainer_email "andrea.campi@zephirworks.com"
+license          "Apache 2.0"
+description      "Deploys and configures Java-based applications"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.1.0"
+
+%w{ application java tomcat }.each do |cb|
+  depends cb
+end
